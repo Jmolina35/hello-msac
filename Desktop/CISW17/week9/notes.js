5b8dc3986d27d1c4de27fc137e1db0d6bca68c2f
Jonathan Molina <jmolina35@student.mtsac.edu>
2024-10-23 20:27:12 -0700

    Add notes.js

---
+++ Desktop/CISW17/week9/notes.js
@@ -0,0 +1,15 @@
+// JavaScript
+
+// Our first JS program
+// Print "Hello, World!" to the console
+
+console.log("Hello, World!");
+
+// Ways to call JS
+//Put it in a separate file
+/* <script src="script.js" defer></script> */
+
+// Use a script tag
+// <script></script>
+
+// Variables
